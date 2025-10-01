9410b333cf92ace1eed0fa5468424ef5f7f6e395
An Vo <35384824+VN-Asianboy@users.noreply.github.com>
2025-10-01 12:49:49 +1000

    ...

--- NibbleKnight2D-main/Assets/Scripts/CoinCollisionScript.cs
+++ NibbleKnight2D-main/Assets/Scripts/CoinCollisionScript.cs
@@ -18,7 +18,19 @@ public class CoinCollisionScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("coin picked up");
-        Destroy(gameObject);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("coin picked up");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("coin picked up");
+            Destroy(gameObject);
+        }
     }
 }
